72cd2cd5d5bd3fd8cfb31c151cfe4611a473a606
[firstname lastname] <[valid-email]>
2025-12-03 13:26:53 +0100

    ukladma ukoly

--- nejaky buttony/js.js
+++ nejaky buttony/js.js
@@ -1,6 +1,34 @@
-addEventlistener("click",funkce)
+let buttonControl = document.getElementById("button1")
+buttonControl.addEventListener("click",funkce)
 
-function funkce(e){
-    console.log(e)
-    console.log(e.target.value)
-}
+function funkce(){
+    let cisla = []
+    let i=0;
+    let x=1;
+    let nejvetsi=0
+    let pocet=0
+    let prumer=0
+    let max=-2147483648, min=21474836948
+    let sudy=0, lichy=0
+        for(i=0;i!=x;x++,i++){
+            cisla[i] = Number(prompt("zadej cislo")) 
+            pocet++
+            if(cisla[i]==0)
+            {
+                i=x;
+            }
+        }
+        for(i=0;i<pocet;i++){
+            if(cisla[i]>max) (max=cisla[i])
+            prumer += cisla[i]
+            if(cisla[i]%2==0) { sudy++  }
+            if(cisla[i]%2!=0) { lichy++ }
+        }
+        pocet=pocet-1
+        console.log("nejvetsi cislo", max)
+        console.log("prumer je", prumer/pocet)
+        console.log("lichy: ", lichy)
+        console.log("sudy:", sudy-1)
+
+        
+    }
